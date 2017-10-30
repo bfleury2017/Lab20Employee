1cb7eb9487d2a0588e0d82f8e8ef022e18ba5807
bfleury2017 <bfleury@outlook.com>
2017-10-29 22:07:10 -0400

    Initial

---
+++ Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Lab20Employee
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private decimal monthlySalary;
+        public decimal Salary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    monthlySalary = value;
+                }
+
+            }
+        }
+
+        public decimal IncreaseSalary() => (Salary * 10 / 100) + Salary;
+
+        public Employee(string first, string last, int salary)
+        {
+            FirstName = first;
+            LastName = last;
+            Salary = salary;
+
+        }
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"{FirstName} {LastName}; yearly salary: {Salary:C}");
+        }
+
+        public void DisplayIncrease()
+        {
+            Console.WriteLine($"{FirstName} {LastName}; yearly salary: {IncreaseSalary():C}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab20Employee
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Bob", "Jones", 34_500);
+            Employee employee2 = new Employee("Susan", "Baker", 37_809);
+
+            Console.WriteLine("Employee salary");
+            employee1.DisplayInformation();
+            employee2.DisplayInformation();
+            Console.WriteLine();
+
+            Console.WriteLine("Increasing employee salary by 10%");
+            employee1.DisplayIncrease();
+            employee2.DisplayIncrease();
+        }
+
+    }
+
+}
